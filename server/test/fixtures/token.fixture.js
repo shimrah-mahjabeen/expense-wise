db55331ec0f750997d7c2f2b0204e24c9ccae1c6
shimrah-mahjabeen <shimrahmahjabeen@gmail.com>
2023-01-03 10:29:00 -0600

    wip

---
+++ server/test/fixtures/token.fixture.js
@@ -0,0 +1,21 @@
+// // eslint-disable-next-line node/no-unpublished-import
+// import moment from "moment";
+// import config from "../../config/config";
+// import { userOne } from "./user.fixture";
+
+// const accessTokenExpires = moment().add(
+//   config.jwt.accessExpirationMinutes,
+//   "minutes",
+// );
+// const userOneAccessToken = tokenService.generateToken(
+//   userOne._id,
+//   accessTokenExpires,
+//   tokenTypes.ACCESS,
+// );
+// // const adminAccessToken = tokenService.generateToken(
+// //   admin._id,
+// //   accessTokenExpires,
+// //   tokenTypes.ACCESS,
+// // );
+
+// export { userOneAccessToken };

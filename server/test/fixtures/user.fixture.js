a27ac95d92f75226d7cd2e5718cbabe0af301380
shimrah-mahjabeen <shimrahmahjabeen@gmail.com>
2023-01-12 19:39:02 +0500

    fix es-lint config message

--- server/test/fixtures/user.fixture.js
+++ server/test/fixtures/user.fixture.js
@@ -7,7 +7,6 @@ import User from "../../models/User";
 const password = "Admin123*";
 const salt = bcrypt.genSaltSync(10);
 const hashedPassword = bcrypt.hashSync(password, salt);
-
 const user = {
   _id: mongoose.Types.ObjectId(),
   firstName: faker.lorem.word(20),
@@ -15,7 +14,6 @@ const user = {
   email: faker.internet.email().toLowerCase(),
   password,
 };
-
 const insertUsers = async (users) => {
   await User.insertMany(
     users.map((_user) => ({ ..._user, password: hashedPassword })),

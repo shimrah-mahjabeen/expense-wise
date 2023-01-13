4d524e0d5026b0c5d5e8e0ee105047331ddb6399
shimrah-mahjabeen <shimrahmahjabeen@gmail.com>
2023-01-13 16:47:44 +0500

    added mui styles for reset password page

---
+++ client/src/pages/resetpassword/resetpassword.styles.ts
@@ -0,0 +1,44 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) => ({
+  container: {
+    width: "50%",
+    flexDirection: "column",
+    display: "flex !important",
+    justifyContent: "center ",
+    alignItems: "center ",
+    [`@media (max-width: 1100px)`]: {
+      width: "70%",
+    },
+    [`@media (max-width: 700px)`]: {
+      width: "90%",
+    },
+  },
+
+  avatar: {
+    margin: '10px !important',
+    backgroundColor: '#ff80ab !important',
+    width: '70px !important',
+    height: '70px !important'
+  },
+
+  icon: {
+    width: '50% !important', height: '50% !important'
+  },
+
+  img: {
+    height: "200px",
+  },
+
+  button: {
+    "&:hover": { backgroundColor: "#f06292 !important" },
+  },
+
+  textField: {
+    "& .MuiOutlinedInput-root:hover": {
+      "& > fieldset": {
+        borderColor: "#f06292",
+      },
+    },
+  },
+}));

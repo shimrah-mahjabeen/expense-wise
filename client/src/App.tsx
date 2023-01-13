895633923ffde00ae1b10705291f47580cb50556
shimrah-mahjabeen <shimrahmahjabeen@gmail.com>
2023-01-13 15:32:28 +0500

    added mui theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { ThemeProvider } from '@mui/material'
 
 import { helloWorldApi } from "./api/helloWorld";
 import logo from './assets/logo.png';
+import { theme } from './theme'
+
 import './App.css';
 
 const App = () => {
@@ -16,12 +19,12 @@ const App = () => {
   }, [data])
 
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This is from backend {data}</h1>
       </header>
-    </div>
+    </ThemeProvider>
   );
 }
 

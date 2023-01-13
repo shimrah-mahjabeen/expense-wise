72daec60bcd26df9ee3a377815ed1c6b10a65c72
shimrah-mahjabeen <shimrahmahjabeen@gmail.com>
2023-01-13 18:26:13 +0500

    rendered component after styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from '@mui/material'
 
-import { helloWorldApi } from "./api/helloWorld";
-import logo from './assets/logo.png';
+import { helloWorldApi } from './api/helloWorld'
+import logo from './assets/logo.png'
 import { theme } from './theme'
 
-import './App.css';
+import './App.css'
+import ResetPasswordPage from 'pages/resetpassword/resetpassword.page'
 
 const App = () => {
   const [data, setData] = useState('')
 
   useEffect(() => {
-
-    helloWorldApi()
-      .then((response) => {
-        setData(response.expensewise)
-      })
+    helloWorldApi().then((response) => {
+      setData(response.expensewise)
+    })
   }, [data])
 
   return (
     <ThemeProvider theme={theme}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>This is from backend {data}</h1>
+        <ResetPasswordPage />
       </header>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App

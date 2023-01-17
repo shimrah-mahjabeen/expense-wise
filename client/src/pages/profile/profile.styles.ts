195b39c033720e8169b22823f12c64dfa4936988
shimrah-mahjabeen <shimrahmahjabeen@gmail.com>
2023-01-17 13:28:00 +0500

    refactor code

--- client/src/pages/profile/profile.styles.ts
+++ client/src/pages/profile/profile.styles.ts
@@ -1,19 +1,19 @@
-import { makeStyles } from '@mui/styles'
+import { makeStyles } from "@mui/styles"
 
 export default makeStyles((theme) => ({
   container: {
     alignContent: 'center',
     borderRadius: 15,
-    width: '40%',
+    width: "40%",
   },
 
   box: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
   },
 
   textfield: {
     borderRadius: 7,
-    backgroundColor: '#eeeeee',
+    backgroundColor: "#eeeeee",
   },
 }))

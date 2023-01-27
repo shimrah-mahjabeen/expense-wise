826e2690a0b22b314872e1623286b89b0f0b13e4
shimrah-mahjabeen <shimrahmahjabeen@gmail.com>
2023-01-27 16:57:01 +0500

    configure router for login signup with mui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,23 +5,24 @@ import { ThemeProvider } from "@mui/material";
 import About from "components/about/About";
 import Expense from "components/expense/Expense";
 import LoginPage from "pages/login/login.page";
-import Navbar from "components/common/layouts/navbar/Navbar";
 import Sheets from "components/sheet/Sheets";
 import { theme } from "theme";
 
 import "App.css";
+import SignupPage from "pages/signup/signup.page";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <header className="App-header">
-        <Navbar />
+        {/* Navbar will be show if user is signed in */}
       </header>
       <div className="App-body">
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/sheets" element={<Sheets />} />
           <Route path="/expenses" element={<Expense />} />
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>

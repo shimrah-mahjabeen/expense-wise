39f2040f8f8f6be584141395cbb94d123c1aaf78
shimrah-mahjabeen <shimrahmahjabeen@gmail.com>
2023-02-06 22:45:39 +0500

    refactor code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,49 +2,27 @@ import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import { ToastContainer } from "react-toastify";
-import { useDispatch } from "react-redux";
 
 import About from "components/about/About";
+import { fetchUserData } from "utils/helpers";
 import { isLoggedIn } from "utils/helpers";
 import LoginPage from "pages/login/login.page";
 import Navbar from "components/common/layouts/navbar/Navbar";
 import Permissions from "components/permissions/Permissions";
 import ProfilePage from "pages/profile/profile.page";
 import ResetPassword from "pages/resetpassword/resetpassword.page";
-import { setCurrentUser } from "slices/userSlice";
 import Sheets from "components/sheet/Sheets";
 import SignupPage from "pages/signup/signup.page";
 import SingleSheet from "components/sheet/ExpenseSheet";
-import useHttp from "utils/useHttp";
 
 import "App.css";
 import { theme } from "theme";
 
 const App = () => {
-  const token = localStorage.getItem("token");
-  const dispatch = useDispatch();
-
-  const { request, error } = useHttp();
-
-  const fetchUserData = async () => {
-    if (token) {
-      const { data } = await request("/auth/me", "GET", token);
-      const currentUser = {
-        id: data._id,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        email: data.email,
-        imageUrl: data.imageUrl,
-      };
-
-      if (!error) {
-        dispatch(setCurrentUser(currentUser));
-      }
-    }
-  };
+  const userLogin = fetchUserData();
 
   useEffect(() => {
-    fetchUserData();
+    userLogin();
   }, []);
 
   return (
--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -1,10 +1,35 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import type { RootState } from "app/store";
+import { setCurrentUser } from "slices/userSlice";
+import useHttp from "./useHttp";
 
 const isLoggedIn = () => {
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
   return currentUser.id === "" ? false : true;
 };
 
-export { isLoggedIn };
+const fetchUserData = () => {
+  const token = localStorage.getItem("token");
+  const dispatch = useDispatch();
+
+  const { request, error } = useHttp();
+
+  return async () => {
+    if (token) {
+      const { data } = await request("/auth/me", "GET", token);
+      const currentUser = {
+        id: data._id,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        imageUrl: data.imageUrl,
+      };
+      if (!error) {
+        dispatch(setCurrentUser(currentUser));
+      }
+    }
+  };
+};
+
+export { isLoggedIn, fetchUserData };
